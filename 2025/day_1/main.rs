f8d847dcf631d7ef862153f79c1fe31d1f0ccbe5
Andrew Antenberg <antenberg@meta.com>
2025-12-01 21:31:13 -0500

    refactor 2025 day 1 to be more like julianna

--- 2025/day_1/main.rs
+++ 2025/day_1/main.rs
@@ -3,14 +3,15 @@ use std::{
     io::{prelude::*, BufReader},
 };
 
-
 const FILENAME: &str = "input.txt";
 const INIT_POS: i32 = 50;
 const NUM_POS: i32 = 100;
 
 fn main() {
-    println!("part_1={}", part_1());
-    println!("part_2={}", part_2());
+    let file_lines = read_file(FILENAME);
+    let parsed_moves = parse_moves(file_lines);
+    let solution = count_zeros(parsed_moves);
+    print_solution(solution);
 }
 
 fn read_file(filepath: &str) -> Vec<String> {
@@ -21,22 +22,11 @@ fn read_file(filepath: &str) -> Vec<String> {
         .collect()
 }
 
-fn part_1() -> i32 {
-    let file_lines = read_file(FILENAME);
-    let lock_moves = parse_moves(file_lines);
-    count_zeros_landed_on(lock_moves)
-}
-
-fn part_2() -> i32 {
-    let file_lines = read_file(FILENAME);
-    let lock_moves = parse_moves(file_lines);
-    count_zeros_crossed(lock_moves)
-}
-
 enum Direction {
     Right,
     Left,
 }
+
 type Move = (Direction, i32);
 
 fn parse_moves(lines: Vec<String>) -> Vec<Move> {
@@ -54,17 +44,28 @@ fn parse_move(line: &str) -> Move {
     (direction, num)
 }
 
-fn count_zeros_landed_on(moves: Vec<Move>) -> i32 {
+
+struct Solution {
+    zeros_landed_on: i32,
+    zeros_crossed: i32,
+}
+
+fn count_zeros(moves: Vec<Move>) -> Solution {
     let mut pos = INIT_POS;
-    let mut zeros = 0;
+    let mut zeros_landed_on = 0;
+    let mut zeros_crossed = 0;
     for lock_move in moves {
-        pos = make_move(pos, &lock_move);
-        pos = wrap_pos(pos);
+        let new_pos = make_move(pos, &lock_move);
+        zeros_crossed += count_zeros_between(pos, new_pos);
+        pos = wrap_pos(new_pos);
         if pos == 0 {
-            zeros += 1;
+            zeros_landed_on += 1;
         }
     }
-    zeros
+    Solution {
+        zeros_landed_on,
+        zeros_crossed
+    }
 }
 
 fn make_move(pos: i32, lock_move: &Move) -> i32 {
@@ -79,17 +80,6 @@ fn wrap_pos(pos: i32) -> i32 {
     if wrapped_pos < 0 { wrapped_pos + NUM_POS } else { wrapped_pos }
 }
 
-fn count_zeros_crossed(moves: Vec<Move>) -> i32 {
-    let mut pos = INIT_POS;
-    let mut zeros_crossed = 0;
-    for lock_move in moves {
-        let new_pos = make_move(pos, &lock_move);
-        zeros_crossed += count_zeros_between(pos, new_pos);
-        pos = wrap_pos(new_pos);
-    }
-    zeros_crossed
-}
-
 fn count_zeros_between(old_pos: i32, new_pos: i32) -> i32 {
     // we know old_pos is in [0, NUM_POS)
     let has_not_crossed_zero = new_pos < old_pos && new_pos > 0;
@@ -98,4 +88,10 @@ fn count_zeros_between(old_pos: i32, new_pos: i32) -> i32 {
     }
     let crosses_zero_downward = old_pos > 0 && new_pos <= 0;
     new_pos.abs() / NUM_POS + if crosses_zero_downward { 1 } else { 0 }
+}
+
+fn print_solution(solution: Solution) {
+    let Solution { zeros_landed_on, zeros_crossed } = solution;
+    println!("part_1={}", zeros_landed_on);
+    println!("part_2={}", zeros_crossed);
 }
